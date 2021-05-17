db58c6b0a12edbb583a211c62e1f3e905fdf41e1
Lucas-2000 <79289422+Lucas-2000@users.noreply.github.com>
2021-05-17 12:30:58 -0300

    Add files via upload

--- src/main/java/banco/Adm.java
+++ src/main/java/banco/Adm.java
@@ -26,7 +26,7 @@ public class Adm {
      */
     public boolean verificaLogin() {
 
-        String sql = "SELECT login, senha FROM tb_cadastro WHERE tipo = 'administrador'";
+        String sql = "SELECT login, senha FROM tb_funcionario WHERE tipo = 'administrador'";
 
         ConexaoBD factory = new ConexaoBD();
 
